0c35339266f0ec3cb745aa54f5cc1ad2e47c42cf
Jepson <szp106920@gmail.com>
2020-11-13 17:37:34 +0800

    完成了展开行中 tag 展示功能。

--- src/components/goods/CategoryParams.vue
+++ src/components/goods/CategoryParams.vue
@@ -36,7 +36,32 @@
 
           <!-- 展示动态参数的表格 -->
           <el-table :data="manyParamsList" border stripe>
-            <el-table-column type="expand"></el-table-column>
+            <el-table-column type="expand">
+              <!-- 展开行中的内容 -->
+              <template slot-scope="scope">
+                <el-tag
+                  v-for="(item, index) in scope.row.attr_vals"
+                  :key="index"
+                  closable
+                >
+                  {{ item }}
+                </el-tag>
+
+                <!-- 添加文本框和按钮 -->
+                <el-input
+                  v-if="scope.row.inputVisible"
+                  v-model="scope.row.inputValue"
+                  ref="saveTagInput"
+                  size="small"
+                  @keyup.enter.native="handleInputConfirm(scope.row)"
+                  @blur="handleInputConfirm(scope.row)"
+                >
+                </el-input>
+                <el-button v-else size="small" @click="showInput(scope.row)"
+                  >+ New Tag</el-button
+                >
+              </template>
+            </el-table-column>
             <el-table-column type="index"></el-table-column>
             <el-table-column
               label="参数名称"
@@ -201,7 +226,7 @@ export default {
     // 获取分类数据列表 [API 1.6.1]
     async getCategoryList() {
       const { data: res } = await this.$http.get('categories')
-      console.log(res)
+      // console.log(res)
       if (res.meta.status !== 200) {
         return this.$message.error(res.meta.msg)
       }
@@ -225,6 +250,18 @@ export default {
       if (res.meta.status !== 200) {
         return this.$message.error('请选择要操作的分类')
       }
+
+      // 返回的数据中，attr_vals 为以空格分隔的字符串，转为数组
+      // 同时使用三元表达式判断，如果 attr_vals 为空字符串，则返回空数组
+      res.data.forEach(item => {
+        item.attr_vals = item.attr_vals ? item.attr_vals.split(' ') : []
+
+        // 控制文本框的显示和隐藏
+        item.inputVisible = false
+        // 文本框的内容
+        item.inputValue = ''
+      })
+
       if (this.activeName === 'many') {
         this.manyParamsList = res.data
       } else {
@@ -307,7 +344,7 @@ export default {
       })
     },
 
-    // 删除动态参数或静态属性
+    // 删除动态参数或静态属性 [API 1.7.3]
     deleteParams(params) {
       this.$msgbox
         .confirm('确定删除？', '提示', {
@@ -331,6 +368,43 @@ export default {
           this.$message.info('已取消')
         })
     },
+
+    // 文本框失去焦点或触发键盘 enter 事件
+    async handleInputConfirm(row) {
+      // 判断文本框输入内容是否为空
+      if (row.inputValue.trim().length === 0) {
+        row.inputValue = ''
+        row.inputVisible = false
+        return
+      }
+
+      // 如果文本框输入内容合法，则将内容传入 attr_vals
+      // 数据准备完毕，即可发起 HTTP 请求 [API 1.7.5]
+      row.attr_vals.push(row.inputValue.trim())
+      row.inputValue = ''
+      row.inputVisible = false
+      const { data: res } = await this.$http.put(
+        `categories/${this.categoryId}/attributes/${row.attr_id}`,
+        {
+          attr_name: row.attr_name,
+          attr_sel: row.attr_sel,
+          attr_vals: row.attr_vals.join(' '),
+          // eslint-disable-next-line comma-dangle
+        }
+      )
+      if (res.meta.status !== 200) {
+        return this.$message.error(res.meta.msg)
+      }
+      this.$message.success(res.meta.msg)
+    },
+
+    // 点击按钮，显示文本框，并自动获取焦点
+    showInput(row) {
+      row.inputVisible = true
+      this.$nextTick(_ => {
+        this.$refs.saveTagInput.$refs.input.focus()
+      })
+    },
   },
 }
 </script>
@@ -338,5 +412,13 @@ export default {
 <style lang="less" scoped>
 .el-cascader {
   margin: 15px 0;
+}
+
+.el-input {
+  width: 120px;
+}
+
+.el-tag {
+  margin-left: 10px;
 }
 </style>
